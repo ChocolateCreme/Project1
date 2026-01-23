890230bd73f0c9ef529c4f2bf1b61cacb889a581
Avani Bhargava <bharga66@purdue.edu>
2026-01-23 17:12:07 -0500

    Lab4

--- Project1/src/App.jsx
+++ Project1/src/App.jsx
@@ -1,21 +1,39 @@
-import Card1 from "./components/Card1"
-import Card2 from "./components/Card2"
-import Navbar from "./components/Navbar"
-import About from "./components/About"
-import './App.css'
+import Card from "./components/Card";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Avani from "./assets/Avani.jpg";
+import Dog from "./assets/Dog.jpg";
+import Wrapper from "./components/Wrapper";
+import "./App.css";
 
 function App() {
+  const profiles = [
+    {id: 0, name: "Avani", title: "Purdue University Student", image: Avani},
+    {id: 1, name: "Dog", title: "Large Stuffed Dog", image: Dog},
+    {id: 2, name: "Avani", title: "Purdue University Student", image: Avani},
+    {id: 3, name: "Dog", title: "Large Stuffed Dog", image: Dog},
+  ]
 
   return (
     <>
       <Navbar />
-      <About />
-      <div className="Profile-card-container">
-        <Card1 />
-        <Card2 />
-      </div>
+      <Wrapper id="about">
+        <About/>
+      </Wrapper>
+      <Wrapper id="profiles">
+        <div className="grid">
+            {profiles.map((profile) => (
+              <Card 
+                key={profile.id}
+                name={profile.name}
+                title={profile.title} 
+                image={profile.image}
+              />
+            ))}
+        </div>
+        </Wrapper>
     </>
-  )
+  );
 }
 
 export default App
