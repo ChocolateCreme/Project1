c96ca5ba694e25a6dfae29f3fce0c530fe39b6a5
Avani Bhargava <bharga66@purdue.edu>
2026-01-28 11:08:45 -0500

    Backup

--- Project1/src/App.jsx
+++ Project1/src/App.jsx
@@ -4,15 +4,43 @@ import About from "./components/About";
 import Avani from "./assets/Avani.jpg";
 import Dog from "./assets/Dog.jpg";
 import Wrapper from "./components/Wrapper";
+import Filters from "./components/Filters";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   const profiles = [
-    {id: 0, name: "Avani", title: "Purdue University Student", image: Avani},
-    {id: 1, name: "Dog", title: "Large Stuffed Dog", image: Dog},
-    {id: 2, name: "Avani", title: "Purdue University Student", image: Avani},
-    {id: 3, name: "Dog", title: "Large Stuffed Dog", image: Dog},
+    {id: 0, name: "Avani", title: "Purdue University student", image: Avani},
+    {id: 1, name: "Dog", title: "Large stuffed dog", image: Dog},
+    {id: 2, name: "Girl", title: "Rocking the ray-bans", image: Avani},
+    {id: 3, name: "Child", title: "Child hugging dog", image: Dog},
   ]
+
+const titles = [...new Set(profiles.map(profile => profile.title))];
+const [clicked, setClicked] = useState(false)
+const handleClick = () => {
+  setClicked (prev => !prev);
+  setClicked (prev => !prev);
+  console.log(clicked);
+};
+const [title, setTitle] = useState("")
+const [name, setName] = useState("")
+const handleChangeTitle = (event) => {
+  setTitle(event.target.value)
+};
+
+const handleSearch = () => {
+  setName(event.target.value)
+};
+
+const handleClear = () => {
+  setTitle("")
+  setName("")
+};
+
+const filteredProfiles = profiles.filter(profile => (
+  (profile.title === title||!title) && (profile.name.toLowerCase().includes(name.toLowerCase()))
+))
 
   return (
     <>
@@ -21,15 +49,27 @@ function App() {
         <About/>
       </Wrapper>
       <Wrapper id="profiles">
+        <Filters 
+        titles={titles}
+        title={title}
+        name={name}
+        handleChange={handleChangeTitle}
+        handleSearch={handleSearch} 
+        handleClick={handleClear}/>
+
         <div className="grid">
-            {profiles.map((profile) => (
+          {filteredProfiles.length > 0? (
+            filteredProfiles.map((profile)=>(
               <Card 
                 key={profile.id}
                 name={profile.name}
                 title={profile.title} 
                 image={profile.image}
               />
-            ))}
+            ))
+          ) : (
+            <p>No profiles matched your search.</p>
+          )}
         </div>
         </Wrapper>
     </>
